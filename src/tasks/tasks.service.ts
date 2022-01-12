b1c932ed6c7d7306bf71a51da1d326c5fcd57e47
diskette42 <supatatdisk@gmail.com>
2022-01-12 14:36:55 +0700

    test

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,27 +7,20 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
-
   getAllTasks(): Task[] {
     return this.tasks;
   }
-
   getTaskWithFilters(filterDto: GetTasksFilterDto): Task[] {
     const { status } = filterDto;
-
     let tasks = this.tasks;
-
     if (status) {
       tasks = this.tasks.filter((task) => task.status === status);
     }
     return tasks;
   }
-
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
-
     console.log(title, description);
-
     const task: Task = {
       id: uuid(),
       title,
@@ -35,12 +28,9 @@ export class TasksService {
       status: TaskStatus.OPEN,
     };
     this.tasks.push(task);
-
     console.log(task);
-
     return task;
   }
-
   getTaskById(id: string): Task {
     let found;
     for (let i = 0; i < this.tasks.length; i++) {
@@ -51,27 +41,22 @@ export class TasksService {
     if (!found) {
       throw new NotFoundException(`Task with id ${id} not found`);
     }
-
     return found;
   }
-
   deleteTaskById(id: string): void {
     // for (let i = 0; i < this.tasks.length; i++) {
     //   if (this.tasks[i].id == id) {
     //     delete this.tasks[i];
     //   }
     // }
-
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
-
   updateTaskStatusById(id: string, status: TaskStatus): Task {
     // for (let i = 0; i < this.tasks.length; i++) {
     //   if (this.tasks[i].id == id) {
     //     this.tasks[i].status = status;
     //   }
     // }
-
     const task = this.getTaskById(id);
     task.status = status;
     return task;
